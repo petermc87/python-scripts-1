afc377c2176828d1f35e187a0b7ffb27a7429e70
petermc87 <peter.mcgibney@gmail.com>
2024-03-19 23:16:01 -0400

    feat: add regex expression

--- email_checker.py
+++ email_checker.py
@@ -1,5 +1,6 @@
 import imapclient
 import pyzmail
+import re
 from bs4 import BeautifulSoup
 
 conn = imapclient.IMAPClient('imap.gmail.com', ssl=True)
@@ -18,12 +19,36 @@ def get_folder(folder):
     # print(messages)
 
 
+def word_match(body, addresses, subject, keyword):
+    
+    # Combine the strings
+    email = f'{addresses} {subject} {body}'
+    
+    # Parsing to check if it exists in the email.
+    # NOTE: The escape is used to escape special characters so that they
+    # dont affect the regular expression syntax. \b is used to search a 
+    # whole word.
+    keywordRegex = re.compile(r'\b' + re.escape(keyword) + r'\b')
+    
+    # Checking for a match
+    match = keywordRegex.search(email)
+    
+    if match:
+        print('Keyword found in: ', subject)
+
+    
+    
+    
+    
+
+
+
 #### FUNCTION TO CHECK EACH MESSAGE ####
-def message_check(messages):
+def message_check(messages, keyword):
 
     for i in messages:
         ### TEST ONLY 3 ###
-        if i < 3:
+        if i < 20:
             # Fetch an email
             rawMessage = conn.fetch([i], ['BODY[]', 'FLAGS'])
             # print(rawMessage)
@@ -32,9 +57,13 @@ def message_check(messages):
             message = pyzmail.PyzMessage.factory(rawMessage[i][b'BODY[]'])
 
             # Get subject, from, to and bcc
-            print(message.get_addresses('from')[0][1], message.get_addresses('to')[0][1], message.get_addresses('bcc'))
-            print(message.get_subject())
-
+            #
+            #### TEST ####
+            # print(message.get_addresses('from')[0][1], message.get_addresses('to')[0][1], message.get_addresses('bcc'))
+            # print(message.get_subject())
+            
+            addresses = message.get_addresses('from')[0][1], message.get_addresses('to')[0][1], message.get_addresses('bcc')
+            subject = message.get_subject()
 
             # Make sure the email is html and not plain text type.
             if message.html_part != None:
@@ -45,10 +74,15 @@ def message_check(messages):
                 text_body = soup.get_text()
                 #
                 ### TODO: USE REGEX TO LOOK FOR WORDS. WHEN MATCHED, DONT DELETE ###
+                # INPUT ASKING FOR A WORD
+                
                 ### TODO: CREATE AN IF BASED ON WHETHER THERE WERE MATHCHES OR NOT.
                 ### IF THERE WERE, DONT DELETE.
+                # print(text_body)
                 #
-                print(text_body)
+                ## Pass in the message body, addresses and subject to the word_match function.
+            
+            word_match(text_body, addresses, subject, keyword)
                 
         else: return 
     
@@ -56,7 +90,10 @@ def message_check(messages):
 
 
 # Create an input to ask to user what folder to search
-inputted_folder = input('What folder do you want to search:')
+inputted_folder = input('What folder do you want to search: ')
+
+# Input asking for keyword
+keyword = input("Enter a keyword: ")
 
 # Convert the input to a string and to uppercase
 converted_input = str(inputted_folder).upper()
@@ -65,7 +102,7 @@ converted_input = str(inputted_folder).upper()
 returned_messages = get_folder(converted_input)
 
 # Return the message information contained within each
-message_check(returned_messages)
+message_check(returned_messages, keyword)
 
 
 
